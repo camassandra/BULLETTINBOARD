355e1090595b6fb8c5bc1a8c74e08cdc61706843
Camassandra <pontecamilla@yahoo.com>
2017-02-21 12:45:38 +0100

    all pages with styling

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
-const pg = require('pg');
+const sequelize = require('sequelize');
 const fs = require('fs');
 var express = require('express');
 const app = express();
 
 
+//here comes the middleware stuff!
+
+app.use(express.static(__dirname+'/includes')) 
+
+//bodyparser here
+//app.use(bodyParser.urlencoded({extended: true}))
+
+app.set('views', __dirname+'/views');
+app.set('view engine', 'pug');
+
+var connString = 'postgres://' + process.env.POSTGRES_USER + ':' + process.env.POSTGRES_PASSWORD + '@localhost/bulletinboard';
+// pg.connect(connectionString, function(err, client, done) {
+
+// }
+
+app.get('/', function(request, response){
+	response.render('bulletinhome')
+})
 
 
 
-
-
-
-
-
-
+app.get('/feeds', function(request, response){
+	response.render('feeds')
+})
 
 
 
